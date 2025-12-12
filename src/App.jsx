704d8276e50421c94107616f46ef48e4e3a63698
samadhan <samadhanbhusnar21@gmail.com>
2025-12-12 19:16:10 +0530

    first commit from musify-admin-app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+ 
+  return (
+    <>
+    <div className="flex items-center justify-center h-screen bg-gradient-to-br from-purple-600 to-blue-500 text-white text-3xl font-bold">
+      <h1>Musify Admin App</h1>
+    </div>
+    </>
+  )
+}
+
+export default App;

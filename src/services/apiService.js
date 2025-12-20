d56050ea3d0c31bedc8533a4694f01c4bc184818
samadhan <samadhanbhusnar21@gmail.com>
2025-12-20 16:09:25 +0530

    url changed

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://musify-app-2.onrender.com";
 export const portal="admin";
 
 // craete axios intance
